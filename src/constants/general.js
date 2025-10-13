c5d47368bad764bb10b6752fda706ea4cfb639bc
German Rodrigo Alvarez Fabre <geralvarez.085@gmail.com>
2025-10-12 22:01:22 -0300

    feat(constants): add file

---
+++ src/constants/general.js
@@ -0,0 +1,4 @@
+export const SERVICE_URL = `https://68d9b3fa90a75154f0dafa92.mockapi.io/api/v1`;
+export const SERIVCE_URL_PRODUCTS = `${SERVICE_URL}/products`;
+
+export const HTML_VERB_GET = 'GET';
